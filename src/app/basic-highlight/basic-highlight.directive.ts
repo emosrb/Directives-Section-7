4f0429538b6ba50f7b380d5ab78abff93c820be4
Saurabh <emosrb@gmail.com>
2020-05-16 19:44:10 +0530

    98-101 Structral directive and ngSwitch

--- src/app/basic-highlight/basic-highlight.directive.ts
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -1,14 +1,13 @@
-import { Directive, ElementRef, OnInit} from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
-    selector: '[appBasicHighlight]'
+  selector: '[appBasicHighlight]'
 })
 export class BasicHighlightDirective implements OnInit {
-    constructor(private elementRef: ElementRef) {
+  constructor(private elementRef: ElementRef) {
+  }
 
-    }
-
-    ngOnInit() {
-        this.elementRef.nativeElement.style.backgroundColor = 'green';
-    }
+  ngOnInit() {
+    this.elementRef.nativeElement.style.backgroundColor = 'green';
+  }
 }
